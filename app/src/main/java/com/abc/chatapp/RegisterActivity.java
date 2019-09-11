96108ba27f3c74c495fcc8942c852e0704e7adc6
shyamanthawijesiri <shyamanthawijesiri33@gmail.com>
2019-09-11 20:06:51 +0530

    add loading to create account

--- app/src/main/java/com/abc/chatapp/RegisterActivity.java
+++ app/src/main/java/com/abc/chatapp/RegisterActivity.java
@@ -1,11 +1,13 @@
 package com.abc.chatapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +25,8 @@ public class RegisterActivity extends AppCompatActivity {
     private Button mCreateBtn;
 
     private Toolbar mToolbar;
+    //progressDialog
+    private ProgressDialog mRegProgress;
     // firebase
     private FirebaseAuth mAuth;
 
@@ -36,6 +40,8 @@ public class RegisterActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Create Account");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mRegProgress = new ProgressDialog(this);
         // firebase
         mAuth = FirebaseAuth.getInstance();
 
@@ -52,7 +58,14 @@ public class RegisterActivity extends AppCompatActivity {
                 String email = mEmail.getEditText().getText().toString();
                 String password = mPassword.getEditText().getText().toString();
 
-                register_user(display_name, email,password);
+                if(!TextUtils.isEmpty(display_name) || !TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)){
+                    mRegProgress.setTitle("Register User");
+                    mRegProgress.setMessage("Please wait while we create your account!");
+                    mRegProgress.setCanceledOnTouchOutside(false);
+                    mRegProgress.show();
+                    register_user(display_name, email,password);
+                }
+
 
             }
         });
@@ -64,11 +77,12 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
+                    mRegProgress.dismiss();
                     Intent mainIntent = new Intent(RegisterActivity.this,MainActivity.class);
                     startActivity(mainIntent);
                     finish();
                 }else{
-
+                    mRegProgress.hide();
                     Toast.makeText(RegisterActivity.this, task.getException().getMessage(),Toast.LENGTH_LONG).show();
                 }
             }
