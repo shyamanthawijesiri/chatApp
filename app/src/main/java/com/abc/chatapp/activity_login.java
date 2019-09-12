c2a2b3e5b402b6be93f453a29144b8b8e1ac54d6
shyamanthawijesiri <shyamanthawijesiri33@gmail.com>
2019-09-12 21:18:26 +0530

    login interface

--- app/src/main/java/com/abc/chatapp/activity_login.java
+++ app/src/main/java/com/abc/chatapp/activity_login.java
@@ -1,13 +1,58 @@
 package com.abc.chatapp;
 
+import android.app.ProgressDialog;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+
+import org.w3c.dom.Text;
 
 public class activity_login extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private TextInputLayout mLoginEmail;
+    private TextInputLayout mLoginPassword;
+    private Button mLogin_btn;
+
+    private ProgressDialog mLoginProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mToolbar = (Toolbar)findViewById(R.id.login_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Login");
+
+        mLoginEmail = (TextInputLayout) findViewById(R.id.login_email);
+        mLoginPassword = (TextInputLayout) findViewById(R.id.login_password);
+        mLogin_btn = (Button) findViewById(R.id.logn_btn);
+
+
+
+        mLogin_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mLoginEmail.getEditText().getText().toString();
+                String password = mLoginPassword.getEditText().getText().toString();
+
+                if(!TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)){
+
+                    mLoginProgress.setTitle("Logged In");
+                    loginUser(email,password);
+                }
+            }
+        });
+
+
+    }
+        private void loginUser(String email, String Password){
+
     }
 }
