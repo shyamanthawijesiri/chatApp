2efeab3ac20f15590766f61db9a66039fa0df012
shyamanthawijesiri <shyamanthawijesiri33@gmail.com>
2020-05-24 17:10:16 +0530

    upload image to firebase

--- app/src/main/java/com/abc/chatapp/SettingsActivity.java
+++ app/src/main/java/com/abc/chatapp/SettingsActivity.java
@@ -1,12 +1,17 @@
 package com.abc.chatapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -14,11 +19,19 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.Random;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsActivity extends AppCompatActivity {
     private DatabaseReference mUserDatabase;
+    private StorageReference mImageStorage;
     private FirebaseUser mCurrentUser;
 
     private CircleImageView mImage;
@@ -26,6 +39,8 @@ public class SettingsActivity extends AppCompatActivity {
     private TextView mStatus;
     private Button mChangeStatus;
     private Button mChangeImage;
+
+    private static final int GELLARY_PICK = 1;
 
 
 
@@ -37,6 +52,9 @@ public class SettingsActivity extends AppCompatActivity {
         mImage = (CircleImageView)findViewById(R.id.settings_image);
         mDisplayName = (TextView)findViewById(R.id.settings_displayName);
         mStatus = (TextView)findViewById(R.id.settings_status);
+        mChangeImage = (Button)findViewById(R.id.setting_image_btn);
+        mChangeStatus = (Button)findViewById(R.id.settings_status_btn);
+        mImageStorage = FirebaseStorage.getInstance().getReference();
 
         mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
 
@@ -62,5 +80,71 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+        mChangeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                CropImage.activity()
+//                        .setGuidelines(CropImageView.Guidelines.ON)
+//                        .start(SettingsActivity.this);
+                Intent galleryIntent = new Intent();
+                galleryIntent.setType("image/*");
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(galleryIntent, "Select Image"),GELLARY_PICK);
+            }
+        });
+
+
+
     }
-}
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+       // Toast.makeText(SettingsActivity.this, requestCode +" "+ requestCode + " "+RESULT_OK, Toast.LENGTH_LONG).show();
+        if(requestCode == GELLARY_PICK && resultCode == RESULT_OK){
+            Uri imageuri = data.getData();
+            CropImage.activity(imageuri)
+                    .setAspectRatio(1,1)
+                    .start(this);
+
+          }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+
+                StorageReference filePath = mImageStorage.child("profile_images").child(random()+".jpg");
+                filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(SettingsActivity.this,"uploaded successfully", Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(SettingsActivity.this,"uploaded failed", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+    }
+
+    public static String random() {
+        Random generator = new Random();
+        StringBuilder randomStringBuilder = new StringBuilder();
+        int randomLength = generator.nextInt(10);
+        char tempChar;
+        for (int i = 0; i < randomLength; i++) {
+            tempChar = (char) (generator.nextInt(96) + 32);
+            randomStringBuilder.append(tempChar);
+        }
+        return randomStringBuilder.toString();
+    }
+    }
